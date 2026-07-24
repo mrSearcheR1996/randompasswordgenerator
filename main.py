8a6b3d61e5e65c35494cd16296830c45e4a72338
mrsearcher <mrsearcherhappy@gmail.com>
2026-07-24 18:28:53 +0000

    add more comment for better readable code

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ class AlfabetPasswordGen(PasswordGen):
     def __init__(self,lenght):
         self.lenght=lenght
 
+    #abstractmethod function
     def password_generator(self):
         return ''.join(choices(string.ascii_letters,k=self.lenght))
 
-
+#add varobale as class im use 35 for 35 character password lenght
 alfapassword=AlfabetPasswordGen(35)
 print(alfapassword.password_generator())
